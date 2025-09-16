43b765d80ac2e92847a732f3cca299ed1b2b1f39
mauro.arduini <mauroo@gmail.com>
2025-09-16 12:42:41 +0200

    fix size volume ebs nat instance

--- main.tf
+++ main.tf
@@ -191,7 +191,7 @@ module "ec2_natgw" {
 
   root_block_device = [
     {
-      volume_size           = 30
+      volume_size           = 20
       volume_type           = "gp3"
       delete_on_termination = true
       encrypted             = true
